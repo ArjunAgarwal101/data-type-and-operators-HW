1966db4d70deb68968cb1f00ff23bf8666a760e0
ArjunAgarwal101 <agarwalarjun101@gmail.com>
2024-12-14 23:43:11 +0530

    hi

---
+++ index.py
@@ -0,0 +1,14 @@
+n1=input("enter number1;")
+n2=input("enter number2;")
+n3=input("enter number3;")
+print("x""=",n1,"y""=",n2,"z""=",n3)
+print()
+print("if we swap x to y and y to z and z to x")
+temp=n1
+n11=n2
+temp=n2
+n2=n3
+temp=n3
+n3=n1
+print("then x=",n11,"y=",n2,"z=",n3)
+
